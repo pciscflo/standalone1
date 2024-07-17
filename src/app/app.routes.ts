6b1dc1ca8aad068af0ab304d20aee6f620e99697
professor <pciscflo@upc.edu.pe>
2024-07-17 02:38:03 -0500

    second commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import {Route, RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
+import {ContactComponent} from "./contact/contact.component";
+import {ProductsComponent} from "./products/products.component";
+import {ProductDetailComponent} from "./product-detail/product-detail.component";
 
-export const routes: Routes = [];
+export const routes: Route[] = [
+  { path: '', component: HomeComponent }, // Página principal
+  { path: 'about', component: AboutComponent }, // Página "About"
+  { path: 'contact', component: ContactComponent }, // Página de contacto
+  { path: 'products', component: ProductsComponent }, // Página de productos
+  { path: 'product/:id', component: ProductDetailComponent }, // Página de detalles del producto
+  { path: '**', redirectTo: '' } // Ruta para manejar rutas no encontradas
+];
